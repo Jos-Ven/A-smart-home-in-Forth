4fd9f3ddc404b260f89e6d205829376bb4c12435
Jos Ven <josv@planet.nl>
2023-12-06 14:41:16 +0100

    Better cleanup of old processes

--- gf.sh
+++ gf.sh
@@ -1,11 +1,10 @@
 date
 
-if [ "$(pidof gforth-fast)" ]
+if [ "$(pidof gforth)" ]
 then
   echo Killing a previous gforth-fast process
-  sudo kill "$(pidof gforth-fast)"
+  sudo kill -9 $(ps aux | grep -e gforth| awk '{ print $2 }')
 fi
-
 
 cd /home/pi
 
@@ -29,12 +28,11 @@ cd /home/pi
         sudo rm -f gf.log
  fi
 
-echo submiting gForth  # Use one of the following lines:
+echo submiting gForth  # Using one of the following lines:
 # sudo nice --10 nohup gforth _demo1.f        1>gf.log 2>&1 &
-# sudo nice --10 nohup gforth _down.fs        1>gf.log 2>&1 &
 # sudo nice --10 nohup gforth _UploadServer.f 1>gf.log 2>&1 &
 # sudo nice --10 nohup gforth _DemoMaster.fs  1>gf.log 2>&1 &
-  sudo nice --10 nohup gforth _SensorWeb1.fs  1>gf.log 2>&1 &
+  sudo nice --10 nohup gforth _SensorWeb1.fs 1>gf.log 2>&1 &
 
 exit 0
 
