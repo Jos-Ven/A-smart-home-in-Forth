31f06e1b84a93a8cb53cd2ab03c1c0eb5c5d3692
Jos Ven <josv@planet.nl>
2024-02-08 16:37:05 +0100

    Improved match-mp and added more documentation.

--- study/window_mpn.f
+++ study/window_mpn.f
@@ -26,7 +26,7 @@ Month	        c	12	0409
 
 2variable gui-mp
 0 gui-mp bInput: i_open       \ 0
-         bInput: i_Override   \ 1
+         bInput: i_Override   \ 1  (inverted i_Automatic)
                  >#bInputs c! \ 2
 
 2variable out-mp
@@ -34,24 +34,24 @@ Month	        c	12	0409
          bInput: i_gui        \ 1
                  >#bInputs c! \ 2
 
-: init-net ( - )  0 autom-mp !   0 gui-mp !   0 out-mp ! ;
+: init-net      ( - )  0 autom-mp !   0 gui-mp !   0 out-mp ! ;
 
-: set-override ( - ) i_Automatic  i_Override   invert-dest-input ;
+: set-override  ( - ) i_Automatic  i_Override   invert-dest-input ;
 
-: eval-wnd-net ( - result-output )     \ Open when true
-   set-override                        \ New input for Destination
+: eval-wnd-net  ( - result-output )  \ Updates the relations. Result: Open window when true
+   set-override                      \ New input for i_Override
    [ autom-mp all-bits ] literal autom-mp match-mp     i_autom  bInput!
    [ gui-mp   all-bits ] literal gui-mp   match-mp     i_gui    bInput!
    out-mp any-mp ;
 
-: .eval-wnd-net   ( - ) \ To Track the inputs and outputs.
-   eval-wnd-net drop
+: .eval-wnd-net ( - )  \ To Track the inputs and outputs.
+   eval-wnd-net drop   \ eval-wnd-net is needed to update the relations!
    cr .line-- space .time
-   cr ." Automation " [ autom-mp all-bits ] literal autom-mp .match-mp
-   cr ." Gui "        [ gui-mp   all-bits ] literal gui-mp   .match-mp
-   cr ." Out "        out-mp .any-mp ;
+   cr ." Autom " [ autom-mp all-bits ] literal autom-mp .match-mp
+   cr ." Gui "   [ gui-mp   all-bits ] literal gui-mp   .match-mp
+   cr ." Out "   out-mp .any-mp ;
 
-init-net .eval-wnd-net
+init-net
 
 0 [if]
     i_Light         bInputOn
@@ -59,17 +59,24 @@ init-net .eval-wnd-net
     i_Temperature   bInputOn
     i_#Changes      bInputOn
     i_OpeningHours  bInputOn
-    i_Month         bInputOn  .eval-wnd-net
+    i_Month         bInputOn  \ of autom-mp
 
-    i_Automatic     bInputOn  .eval-wnd-net
+ autom-mp .inputs-mp
+   gui-mp .inputs-mp
+   out-mp .inputs-mp
+ .eval-wnd-net
 
-    i_Temperature   bInputOff .eval-wnd-net
+    i_Automatic     bInputOn    .eval-wnd-net
+
+\ autom-mp .inputs-mp
+
+    i_Temperature   bInputOff   .eval-wnd-net
 
     i_Automatic bInputoff .eval-wnd-net
-    i_open      bInputOn  .eval-wnd-net
+    i_open      bInputOn  .eval-wnd-net \ of gui-mp
     i_open      bInputOff .eval-wnd-net
 
-  eval-wnd-net .
+\  eval-wnd-net .
 [then]
 
-
+cr .( window_mpn.f compiled)
